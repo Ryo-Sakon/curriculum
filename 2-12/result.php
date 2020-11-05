8ca25458a1f9f6c74b490f603c8cd97f1b4cfb56
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-11-05 19:09:29 +0900

    2-12

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$prize = $_POST['prize'];
+$numberOfItems = $_POST['numberOfItems'];
+?>
+<p>お名前：<?php echo $my_name; ?></p>
+<p>ご希望景品：<?php echo $prize; ?></p>
+<p>個数：<?php echo $numberOfItems; ?>個</p>
