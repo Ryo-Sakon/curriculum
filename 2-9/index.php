ea58189478dc1341ff4b2affcdf5e1cca2fed91c
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-11-08 13:34:49 +0900

    Update index.php

--- 2-9/index.php
+++ 2-9/index.php
@@ -1,12 +1,12 @@
 <?PHP
     $num = 1;
     while ($num < 100) {
-        if ($num % 3 == 0 && $num % 5 != 0) {
-            echo "Fizz!!";
-        } elseif ($num % 3 != 0 && $num % 5 == 0) {
-            echo "Buzz!!";
-        } elseif ($num % 3 == 0 && $num % 5 == 0) {
+        if ($num % 3 == 0 && $num % 5 == 0) {
             echo "FizzBuzz!!";
+        } elseif ($num % 3 == 0) {
+            echo "Fizz!!";
+        } elseif ($num % 5 == 0) {
+            echo "Buzz!!";
         }else{
             echo $num;
         }
