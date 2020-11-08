486e7dc9b8a65973b48b5ef5754f0be9bd6b59be
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-11-08 13:23:00 +0900

    課題

---
+++ 2-17/index.php
@@ -0,0 +1,26 @@
+<?php
+    $counts = 0;
+    $sum = 0;
+    while ($sum <= 40) {
+        $counts++;
+        $num = mt_rand(1,6);
+        $sum += $num;
+        echo "{$counts}回目＝{$num}";
+        echo "<br>";
+    }
+    echo "{$counts}回目でゴールしました";
+
+    echo "<br><br><br>";
+
+    echo "今".date("h")."時台です";
+    echo "<br>";
+    $date = date("h");
+    if ($date >= 4 && $date < 10) {
+        echo "おはようございます";
+        
+    }elseif ($date >= 10 && $date < 18) {
+        echo "こんにちは";
+    }else{
+        echo "こんばんは";
+    }
+?>
