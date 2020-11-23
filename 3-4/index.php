2aa5b30792cee2a1b1021e4963465b583a0e9258
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2020-11-23 11:52:04 +0900

    a

---
+++ 3-4/index.php
@@ -0,0 +1,29 @@
+<!-- <?php
+require_once("getData");
+
+?> -->
+<!DOCTYPE html>
+<html lang="ja">
+<!-- <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head> -->
+<body>
+    <header>
+        <div class="header-left">
+            <img src="img/yigrouplogo.png">
+        </div>
+        <div class="header-right">
+            <p>ようこそさん</p>
+            <p>最終ログイン日</p>
+        </div>
+    </header>
+    <div class="main">
+
+    </div>
+    <footer>Y&I group.inc</footer>
+</body>
+</html>
--- 3-4/pdo.php
+++ 3-4/pdo.php
@@ -1,20 +1,16 @@
 <?php 
-$dbName="webtube_db";
-$host="localhost";
-$dsn = 'mysql:dbname=$dbName;host=$host;charset=utf8mb4';
-$username = 'root';
-$password = 'password';あとで確認
-try {
-    $pdo=new PDO($dsn,$username,$password);
-    
-} catch (\Throwable $th) {
-    //throw $th;
+function connect(){
+    $dbName="checktest4";
+    $host="localhost";
+    $dsn = sprintf('mysql:dbname=%s;host=%s;charset=utf8mb4', $dbName, $host);
+    $username = 'root';
+    $password = 'root';
+    try {
+        $dbh=new PDO($dsn, $username, $password);
+        echo "接続できました";
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
 }
-
 ?>
-
-
-<!-- php data objects -->
-PDOはmysqlだけじゃなく、どのDBにも使える
-＄dsnはデータベースの詳細
-tryは処理でエラーが発生したら、catchを出力sるう
