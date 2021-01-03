6f80a284e040a8a154cce3c44460982271bfd44c
Ryo-Sakon <68645178+Ryo-Sakon@users.noreply.github.com>
2021-01-03 16:37:10 +0900

    a

--- Practice/ryo.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$fruits=["apple","melon"];
-echo $fruits[0].$fruits[1];
-?>
--- Practice/udon_insert.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $pdo = new PDO("mysql:dbname=men","root","root");
-    $st = $pdo->prepare("INSERT INTO udon VALUES(?,?)");
-    $st->execute(array($_POST['name'],$_POST['price']));
-?>
-レコードを追加しました。
